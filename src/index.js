d6a14d25a7d45a45bf5cf8afdfa3353b230888f4
Blue Linden <dev@bluelinden.art>
2023-02-11 12:50:14 +0000

    using SASS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import {Host, User} from './connect.mjs'; // eslint-disable-line no-unused-vars
 import {Game} from './game-starter.mjs'; // eslint-disable-line no-unused-vars
 import Peer from 'peerjs'; // eslint-disable-line no-unused-vars
+import { } from './styles/cover.sass'; // eslint-disable-line no-unused-vars
 const testHost = new Host('test');
 const testPlayer = new User('test');
 const testPeer = new Peer('wahahhahahahahaha');
