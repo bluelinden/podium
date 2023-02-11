7aa2d3c9cea367c21113b113f87c4fc25035d8bb
Blue Linden <dev@bluelinden.art>
2023-02-11 01:47:20 +0000

    Creating host class

--- connect.mjs
+++ connect.mjs
@@ -1,4 +1,20 @@
 'use strict';
+/* eslint-disable max-len */
 import Peer from 'peerjs';
-let myID = 'bl-podium-' + Math.random().toString(36).substring(2, 10);
-const me = new Peer();
+import adapter from 'webrtc-adapter'; // eslint-disable-line no-unused-vars
+export {Host};
+
+/**
+ * @class Host
+ * @classdesc A class that represents a game host.
+ */
+class Host {
+  /**
+   * @constructor
+   * @description Creates a new host.
+   */
+  constructor() {
+    const peerID = 'bl-podium-host-' + Math.random().toString(36).substring(2, 15);
+    this.peer = new Peer(peerID);
+  }
+}
--- readme.md
+++ readme.md
@@ -1,1 +1,6 @@
-# Hello.
+# Podium: The Free Live Trivia Game
+---
+
+Podium is a free, interactive trivia game to play with your friends, classmates, really whoever can stand to have a good time without being bugged to create an account.
+
+Games are joined with a five-character join code, 
