8b78f945c0f75abdab999acadaa5c5f0c04a7f77
Blue Linden <dev@bluelinden.art>
2023-02-15 02:59:38 +0000

    Adding comments

--- src/event-bus.mjs
+++ src/event-bus.mjs
@@ -4,14 +4,15 @@ import('js-event-bus');
 
 The Podium Event Manager system - Scalability, reliability, and performance. Without being horrible to maintain.
 
-All events within Podium fall into one of seven categories:
+All events within Podium fall into one of eight categories:
 * General - Events that are not specific to any particular part of the system
 * Display - Events that are specific to layout and modals
-* Game - Events that are specific to the game itself, like score and questions
+* Game - Events that are specific to the game itself, like score, question parsing, and rank calculation.
 * Transport - Events that are specific to the transport layer, like connection and disconnection, and failover management.
 * Stage - Events that are specific to the stage, like host changeovers and teammates.
-* Interact - Events that are specific to communications directly with other users, like requesting a host changeover or messaging a user.
+* Interact - Events that are specific to communications directly with other users, like requesting a host or relay changeover, submitting an answer, or messaging someone else.
 * Broadcast - Events that are broadcasted to all users, like enacting a host changeover or broadcasting a message to all users.
+* Heartbeat - System integrity stuff, like checking if the client is connected to the host, doing periodic mesh stability checks, performing anti-cheat checks, and syncing game state with the host once in a while.
 
 */
 /**
@@ -30,5 +31,6 @@ export default class Bus {
     this.stage = new EventBus();
     this.interact = new EventBus();
     this.broadcast = new EventBus();
+    this.heartbeat = new EventBus();
   }
 }
